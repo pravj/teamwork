b49d59d834e9db0d1a656635855f1ba4b730001b
Pravendra Singh <hackpravj@gmail.com>
2014-08-07 22:55:44 +0530

    Crawler module : add method to save members
    > save public members of organization in 'data/members.json'

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,4 +1,6 @@
+import os
 import sys
+import json
 
 try:
     import requests
@@ -11,12 +13,40 @@ class Crawler:
     def __init__(self, org):
         self.org = org
 
-    def isOrg(self):
-        url = "https://api.github.com/users/%s" % (self.org)
-        res = requests.get(url)
+    # checks GitHub user type for 'Organization' or 'User' type
+    #
+    # return [boolean]
+    def is_org(self):
+        user_url = "https://api.github.com/users/%s" % (self.org)
+        res = requests.get(user_url)
 
-        if (res.status_code == 200):
+        if (res.status_code == requests.codes.ok):
             return (res.json()['type'] == u'Organization')
         else:
             print res.json()['message']
             sys.exit(0)
+
+    # collects public members for a GitHub organization
+    def members(self):
+        members_url = "https://api.github.com/orgs/%s/public_members"\
+            % (self.org)
+        res = requests.get(members_url)
+
+        if (res.status_code == requests.codes.ok):
+            self.add_members(res.json())
+        else:
+            print res.json()['message']
+            sys.exit(0)
+
+    # saves members in 'members.json' inside 'data' directory
+    def add_members(self, response):
+        members = []
+        for member in response:
+            members.append(member['login'])
+
+        path = os.path.join(os.path.dirname(__file__), '../data/members.json')
+        members_file = os.path.abspath(path)
+
+        with open(members_file, 'w') as f:
+            f.write(json.dumps(members))
+            f.close()
