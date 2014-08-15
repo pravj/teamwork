09b031b0be639ee13c287a34f7befc4f02aa56f7
Pravendra Singh <hackpravj@gmail.com>
2014-08-15 23:08:24 +0530

    Driver module : populating driver API

--- db/driver.py
+++ db/driver.py
@@ -1,8 +1,9 @@
 import os
-import json
+import ast
 import rethinkdb as r
 
-CONFIG_PATH = os.path.join(os.path.dirname(__file__), '../config/teamwork.json')
+CONFIG_PATH = os.path.join(
+    os.path.dirname(__file__), '../config/teamwork.json')
 CONFIG_FILE = os.path.abspath(CONFIG_PATH)
 
 
@@ -14,9 +15,13 @@ class Driver:
         self.db = None
         self.auth_key = None
 
+        self.con = None
+
+        self.load()
+
     def load(self):
         with open(CONFIG_FILE, 'r') as f:
-            config = json.loads(f.read())
+            config = ast.literal_eval(f.read())
             f.close()
 
         self.host = config['host']
@@ -25,4 +30,36 @@ class Driver:
         self.auth_key = config['auth_key']
 
     def connect(self):
-        return r.connect(host=self.host, port=self.port, db=self.db, auth_key=self.auth_key)
+        self.con = r.connect(host=self.host,
+                             port=self.port,
+                             db=self.db,
+                             auth_key=self.auth_key).repl()
+
+        self.create_db()
+
+    def create_db(self):
+        db_list = r.db_list().run()
+
+        if (self.db in db_list):
+            pass
+        else:
+            r.db_create(self.db).run()
+
+    def table_exist(self, table):
+        table_list = r.table_list().run()
+
+        if (table in table_list()):
+            return True
+        else:
+            return False
+
+    def create_table(self, table):
+        r.table_create(table).run()
+
+    def insert(self, table, data):
+        if (self.table_exist(table)):
+            pass
+        else:
+            self.create_table(table)
+
+        r.table(table).insert(data).run()
