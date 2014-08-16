e0219cffc3681d6acc11a27106e6baa83b2bc586
Pravendra Singh <hackpravj@gmail.com>
2014-08-16 20:31:50 +0530

    Query module : include commit actor as user field

--- crawler/query/builder.py
+++ crawler/query/builder.py
@@ -12,8 +12,8 @@ class Builder:
 
     def build(self, member, end=None):
         query = """SELECT payload_head, repository_name, repository_language,
-        repository_size, repository_pushed_at FROM
-        [githubarchive:github.timeline] WHERE type='PushEvent' AND
+        repository_size, repository_pushed_at, actor_attributes_login as user
+        FROM [githubarchive:github.timeline] WHERE type='PushEvent' AND
         repository_organization='%s' AND actor_attributes_login='%s' AND
         PARSE_UTC_USEC(created_at)>=PARSE_UTC_USEC('%s')
         """ % (self.org, member, self.start())
