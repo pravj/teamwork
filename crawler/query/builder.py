0a7d801dd749317b126dd3db2d15b8898a9e2cc4
Pravendra Singh <hackpravj@gmail.com>
2014-08-17 17:00:46 +0530

    Builder module : change query structure
    > in order to make things fast
    
    > now it asks for each PushEvent in org's repos and filters out
      activity by non organization members

--- crawler/query/builder.py
+++ crawler/query/builder.py
@@ -10,12 +10,13 @@ class Builder:
     def start(self):
         return "%s-%s-%s 00:00:00" % (PAST.year, PAST.month, PAST.day)
 
-    def build(self, member, end=None):
+    def build(self, end=None):
         query = """SELECT payload_head, repository_name, repository_language,
         repository_size, repository_pushed_at, actor_attributes_login as user
         FROM [githubarchive:github.timeline] WHERE type='PushEvent' AND
-        repository_organization='%s' AND actor_attributes_login='%s' AND
+        repository_organization='%s' AND
         PARSE_UTC_USEC(created_at)>=PARSE_UTC_USEC('%s')
+        ORDER BY repository_pushed_at ASC
         """ % (self.org, member, self.start())
 
         if end is not None:
