46db9fc9cffcb6aa6a790f1c1ac8d623dc7b750e
Nairitya Khilari <nkmann2@gmail.com>
2014-08-21 17:23:11 +0530

    Modify : css modified for calendar view
    made calendar default page.

--- teamwork/routes/__init__.py
+++ teamwork/routes/__init__.py
@@ -1,27 +1,38 @@
 from teamwork import app
 from flask import Flask, jsonify, request, render_template
 from teamwork.engine.procer import Procer
+# from teamwork.engine.api import Api
+
+procer = Procer()
+procer.connect()
+org = procer.org
+avatar_url = procer.get_gravatar()
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def cal():
+	if(org is None or org == ''):
+		return render_template("sam.html")
+	return render_template("calender.html", org=org)
 
-@app.route('/home')
-def home():
-	return render_template("sam.html");
 
+@app.route('/api/org')
+def org_detail():
+	print avatar_url
+	return avatar_url
+	# return Api().detail_org(org)
+
+"""
 @app.route('/calender', methods=['GET','POST'])
 def calenderData():
 	org = request.form['org']
 	if(org is None or org == ''):
 		return render_template("sam.html")
 	return render_template("calender.html", org=org)
+"""
 
 @app.route('/calender/details', methods=['POST'])
 def return_details():
 	org = request.form['org']
 	if(org is None or org == ''):
 		return render_template("sam.html")
-	procer = Procer()
-	procer.connect()
-	return jsonify(procer.getCalenderData(org))
+	return jsonify(procer.get_calender_data())
