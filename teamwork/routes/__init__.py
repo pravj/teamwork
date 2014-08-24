0758b0133fce77c295663494986dc684dd8c8a1f
Nairitya Khilari <nkmann2@gmail.com>
2014-08-24 16:50:42 +0530

    Update : Html content to jinja2

--- teamwork/routes/__init__.py
+++ teamwork/routes/__init__.py
@@ -1,8 +1,26 @@
+import os
 from teamwork import app
 from flask import Flask, jsonify, request, render_template
 from db.driver import Driver
 from config import configer
 import json
+
+INFO_PATH = os.path.join(os.path.dirname(__file__), './../../raw/info.json')
+info_file = os.path.abspath(INFO_PATH)
+
+MEMBER_PATH = os.path.join(os.path.dirname(__file__), './../../raw/members.json')
+member_file = os.path.abspath(MEMBER_PATH)
+
+with open(info_file, 'r') as f:
+	_info = f.read()
+	f.close()
+
+with open(member_file, 'r') as f:
+	member = f.read()
+	f.close()
+
+info = json.loads(_info)
+info["public_member"] = len(json.loads(member));
 
 config = configer.config('teamwork')
 d = Driver(config)
@@ -13,7 +31,7 @@ d.connect()
 def cal():
 	_d = json.loads(d.table_data("repositories", 5, 0, "repo_commit", "repo_name" ,"repo_commit"))
 	__d = json.loads(d.table_data("contributions", 6, 0, "total", "total", "reference"))
-	return render_template("calender.html", org=config['organization'], repos=_d, users=__d)
+	return render_template("teamwork.html", org=config['organization'], repos=_d, users=__d, info=info)
 
 @app.route('/calender')
 def calender():
